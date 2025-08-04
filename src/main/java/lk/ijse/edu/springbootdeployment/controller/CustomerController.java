9c7d4a1e854d94a56a9ac7cafe0c88cc999af39f
KaveeN <vpdkkaveen@gmail.com>
2025-08-04 11:30:38 +0530

    Update database configuration with new credentials and connection URL

--- src/main/java/lk/ijse/edu/springbootdeployment/controller/CustomerController.java
+++ src/main/java/lk/ijse/edu/springbootdeployment/controller/CustomerController.java
@@ -30,7 +30,7 @@ public class CustomerController {
         return "Customer data retrieved successfully!";
     }
 
-    @PostMapping("/save")
+    @PostMapping
     public ResponseEntity<ApiResponse<CustomerDTO>> saveCustomer(@RequestBody CustomerDTO customer) {
         return new ResponseEntity<>(new ApiResponse<>(
                 200,
