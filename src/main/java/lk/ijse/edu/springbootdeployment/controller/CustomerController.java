9ce1d57e5a7bea3a5ecaa723657d9e27aa936e4c
KaveeN <vpdkkaveen@gmail.com>
2025-08-04 10:01:50 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/edu/springbootdeployment/controller/CustomerController.java
@@ -0,0 +1,14 @@
+package lk.ijse.edu.springbootdeployment.controller;
+
+/**
+ * --------------------------------------------
+ * @Author Dimantha Kaveen
+ * @GitHub: https://github.com/KaveenDK
+ * --------------------------------------------
+ * @Created 8/4/2025
+ * @Project SpringBootDeployment
+ * --------------------------------------------
+ **/
+ 
+  public class CustomerController {
+}
